ea7ef4a950e247cfa9f3e63725fbd08958199f68
tutizaraz <dbarabashdev@gmail.com>
2023-07-30 02:45:15 +0200

    feat: Add salted hashing functionality with iterations

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const SHA256 = "sha256";
 const MD5 = "md5";
 
 const HashUtility = {
+  saltedHash: (
+    input,
+    algorithm = SHA256,
+    saltLength = 16,
+    iterations = 1000
+  ) => {
+    if (![SHA256, MD5].includes(algorithm)) {
+      throw new Error(`Unsupported algorithm: ${algorithm}`);
+    }
+    const salt = crypto.randomBytes(saltLength).toString("hex");
+    const hash = crypto.pbkdf2Sync(input, salt, iterations, 64, algorithm);
+    return {
+      salt,
+      hash: hash.toString("hex"),
+    };
+  },
   stringHash: (input, algorithm = SHA256) => {
     if (![SHA256, MD5].includes(algorithm)) {
       throw new Error(`Unsupported algorithm: ${algorithm}`);
